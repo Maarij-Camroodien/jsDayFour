2a8542900eba25b2c56c1f6b3b035fa4fb1a3a31
Maarij-Camroodien <maarijcamroodien18@gmail.com>
2023-05-19 09:14:03 +0200

    final push

---
+++ script/code.js
@@ -0,0 +1,16 @@
+let justin = {
+    firstName: "Justin",
+    lastName: 'sam',
+    age: 32
+}
+console.log(justin.firstName);
+
+let maarij = {
+    firstName: "Maarij",
+    lastName: 'Camroodien',
+    age: 30000
+}
+console.log(maarij.lastName);
+
+let names = [maarij]
+console.log(imad);
